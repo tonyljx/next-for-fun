3023d00c60f052b0bbf97a35787bd69434c4504f
liangjiongxin <liangjiongxin@baidu.com>
2023-11-06 10:54:05 +0800

    feature: nav-sidebar

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+## 技术栈
+
+- 图标库: lucide-react
+- ui库: shadcn/ui
+- 框架: nextjs
+
+---
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/components/global/nav-sidebar.tsx
+++ src/components/global/nav-sidebar.tsx
@@ -3,6 +3,13 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import {
+  User2,
+  MonitorCheck,
+  BellDot,
+  FileBarChart2,
+  Coffee,
+} from "lucide-react";
 
 type Props = {
   items: {
@@ -15,23 +22,32 @@ export default function SideBarNav({ items, className, ...props }: Props) {
   const pathName = usePathname();
   console.log(pathName);
 
+  const icons = [
+    <User2 key="profile" />,
+    <Coffee key="account" />,
+    <MonitorCheck key="appearance" />,
+    <BellDot key="notification" />,
+    <FileBarChart2 key="display" />,
+  ];
+
   return (
     <nav
-      className={cn("  hidden py-2 md:block md:w-1/5", className)}
+      className={cn("hidden py-2 md:block md:w-[200px]", className)}
       {...props}
     >
       <div className="flex flex-col space-y-3 ">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <Link
             className={cn(
-              "rounded-md px-[10px] py-2 hover:bg-slate-100 dark:text-slate-600",
+              "flex items-center gap-2 rounded-md px-[10px] py-2 hover:bg-slate-100",
               {
-                "bg-slate-100": item.href === pathName,
+                "bg-slate-100 text-blue-500": item.href === pathName,
               },
             )}
             key={item.name}
             href={item.href}
           >
+            {icons[index % icons.length]}
             {item.name}
           </Link>
         ))}
